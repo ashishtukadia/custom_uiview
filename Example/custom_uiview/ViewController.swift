723792007f2dfcfcec91448e80a965296c0b8310
AshishT <ashishtukadia3609@gmail.com>
2025-11-07 17:08:09 +0530

    Add Example

--- Example/custom_uiview/ViewController.swift
+++ Example/custom_uiview/ViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
+import custom_uiview
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var demoView: UIView!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            self.demoView.makeRound()
+            self.demoView.borderWidth = 2
+            self.demoView.borderColor = .systemBlue
+            self.demoView.applyShadow()
+        }
 
 }
 
